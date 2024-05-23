ee7335ab6a37a3aec97beea3bc1b730c6434bc6d
Aahmed-Hossain <aahmdctec@gmail.com>
2024-05-24 02:47:15 +0600

    feature: login option created with Mui and react hook form

--- client/src/routes/MainRoutes.jsx
+++ client/src/routes/MainRoutes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import ErrorPage from "../pages/ErrorPage";
 import Home from "../pages/Home";
+import Login from "../pages/Login";
 
 const MainRoutes = createBrowserRouter([
   {
@@ -12,9 +13,15 @@ const MainRoutes = createBrowserRouter([
         {
             path: '/',
             element: <Home/>
-        }
+        },
+      
 
     ],
   },
+  {
+    path:'/login',
+    element: <Login></Login>
+},
+
 ]);
 export default MainRoutes;
