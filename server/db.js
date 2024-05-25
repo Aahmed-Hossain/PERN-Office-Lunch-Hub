f39d201455e8a6c9da578a353d0b92d520f894c8
Aahmed-Hossain <aahmdctec@gmail.com>
2024-05-25 20:23:36 +0600

    api created for meals using the postman

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
-user : 'ahmed',
-password: 'muhammad',
+user : 'lunchhubuser',
+password: 'lunchHubUser@123',
 host: 'localhost',
 port: 5432,
 database : 'lunchhub'
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const pool = require('./db')
 require('dotenv').config();
 const port = process.env.PORT || 3001;
 
@@ -11,16 +12,61 @@ app.use(express.json())
 // ROUTES
 
 // create a lunch menu/meal;;
-
 app.post("/meals", async(req, res)=> {
     try {
-        console.log(req.body);
-        res.send('Lunch menu/meal created successfully!')
+        const { name, items, description, price, image, calories, protein, fats, carbs } = req.body;
+        const values = [name, items, description, price, image, calories, protein, fats, carbs];
+        const columns = "name, items, description, price, image, calories, protein, fats, carbs";
+        const placeholders = "$1, $2, $3, $4, $5, $6, $7, $8, $9";
+        const query = `INSERT INTO meal (${columns}) VALUES(${placeholders}) RETURNING *`;
+        const newMeal = await pool.query(query,  values
+        )
+        res.json(newMeal.rows[0])
     } catch (error) {
         console.error(error.message);
         res.status(500).send('Error creating lunch menu/meal')
     }
+});
+
+// get all lunch menu/meal;
+app.get('/meals', async(req, res)=> {
+    try {
+        const allMeals = await pool.query("SELECT * FROM meal");
+        res.json(allMeals.rows)
+    } catch (err) {
+        console.error(err.message)
+    }
+});
+
+// get single meal 
+app.get('/meals/:id', async(req, res)=>{
+    try {
+        const {id} = req.params;
+        const meal = await pool.query("SELECT * FROM meal WHERE meal_id=$1",[id]);
+        res.json(meal.rows[0]);
+    } catch (err) {
+        console.error(err.message)
+    }
+});
+// delete a meal;
+app.delete('/meals/:id', async(req,res)=> {
+const {id} = req.params;
+const deletedMeal = await pool.query('DELETE FROM meal WHERE meal_id=$1',[id]);
+res.json('Your Meal Deleted Successfully');
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send(`Server Running on port: ${port}`)
