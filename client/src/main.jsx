68659dcd0018c3ee0cc833bf6e5bc43fed570455
Aahmed-Hossain <aahmdctec@gmail.com>
2024-05-26 16:41:44 +0600

    feat: installed and setup the providers , tanstack query, axios, react-toastify

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import { BrowserRouter } from 'react-router-dom';
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import MainRoutes from './routes/MainRoutes'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import AuthProvider from '../src/providers/AuthProvider';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+const queryClient = new QueryClient()
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-<RouterProvider router={MainRoutes}/>
+
+<QueryClientProvider client={queryClient}>
+    <AuthProvider>
+    <RouterProvider router={MainRoutes}></RouterProvider>
+    </AuthProvider>
+    </QueryClientProvider>
+
+<ToastContainer 
+position="top-right"
+autoClose={3000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light" />
   </React.StrictMode>,
 )
--- client/src/providers/AuthProvider.jsx
+++ client/src/providers/AuthProvider.jsx
@@ -1,20 +1,20 @@
 /* eslint-disable react/prop-types */
 
 import { createContext, useState } from "react";
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
    const logoutUser = () => {
         setLoading(true)
         setUser(null);
         localStorage.removeItem('token');
-        navigate('/login');
+        // navigate('/login');
         toast.success(`Log out successfully!`)
       }; 
       
