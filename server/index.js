a769115093553b2a344efe65b27134d30f0a8954
Aahmed-Hossain <aahmdctec@gmail.com>
2024-05-29 14:27:19 +0600

    api updated so that same user can't select same meal multiple times

--- server/index.js
+++ server/index.js
@@ -62,9 +62,18 @@ res.json('Your Meal Deleted Successfully');
 app.post("/selectedMeals", async(req, res)=> {
     try {
         const { name, email, price, image, date } = req.body;
+        const checkQuery = `SELECT * FROM selectedmeals WHERE name = $1 AND email = $2 AND date = $3`;
+        const checkValues = [name, email, date];
+        const alreadySelectedMeal = await pool.query(checkQuery, checkValues);
+
+        if (alreadySelectedMeal.rows.length > 0) {
+            // User has already selected this meal
+            return res.status(400).json({ message: 'You have already selected this meal.' });
+        }
         const values = [name, email, price, image, date];
         const columns = "name, email, price, image, date";
         const placeholders = "$1, $2, $3, $4, $5";
+        // const alreadySelectedMeal =
         const query = `INSERT INTO selectedmeals (${columns}) VALUES(${placeholders}) RETURNING *`;
         const selectedMeals = await pool.query(query,  values
         )
@@ -75,6 +84,15 @@ app.post("/selectedMeals", async(req, res)=> {
     }
 });
 // get the selected meal;
+app.get('/selectedMeals/:email', async(req, res)=> {
+    try {
+        const {email} = req.params;
+        const selectedMeals = await pool.query("SELECT * FROM selectedmeals WHERE email=$1",[email]);
+        res.json(selectedMeals.rows)
+    } catch (err) {
+        console.error(err.message)
+    }
+});
 app.get('/selectedMeals', async(req, res)=> {
     try {
         const selectedMeals = await pool.query("SELECT * FROM selectedmeals");
