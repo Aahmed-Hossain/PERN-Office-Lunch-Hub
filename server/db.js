ef2beacfe658535a1e948e517e6b56589a146d9d
Aahmed-Hossain <aahmdctec@gmail.com>
2024-05-30 17:38:42 +0600

    ready to deploy :

--- server/db.js
+++ server/db.js
@@ -1,9 +1,19 @@
+
+// const Pool = require('pg').Pool;
+// const pool = new Pool({
+// user : 'lunchhubuser',
+// password: 'lunchHubUser@123',
+// host: 'aws-0-ap-southeast-1.pooler.supabase.com',
+// port: 5432,
+// database : 'postgres'
+// });
+// module.exports = pool;
+
+
 const Pool = require('pg').Pool;
+
 const pool = new Pool({
-user : 'lunchhubuser',
-password: 'lunchHubUser@123',
-host: 'localhost',
-port: 5432,
-database : 'lunchhub'
+  connectionString: process.env.DB_URI
 });
+
 module.exports = pool;
